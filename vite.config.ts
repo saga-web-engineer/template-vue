fd930bf8ffba38b1259162d414ca81465b616e7f
kumeyuta <ku8.m.1.e2@gmail.com>
2024-10-19 02:10:37 +0900

    import文の置き換え

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { fileURLToPath, URL } from 'node:url';
 
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
-import VueDevTools from 'vite-plugin-vue-devtools';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueDevTools()],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
