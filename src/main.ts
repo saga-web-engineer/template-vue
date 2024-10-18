454d892074caf0fa8cade98132d55d0e27a155b2
kumeyuta <ku8.m.1.e2@gmail.com>
2024-10-19 02:10:07 +0900

    パスを@へ置き換え

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { createHead } from '@unhead/vue';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 
+import App from '@/App.vue';
+import router from '@/router';
 import '@/styles/style.scss';
-import App from './App.vue';
-import router from './router';
 
 const app = createApp(App);
 const head = createHead();
